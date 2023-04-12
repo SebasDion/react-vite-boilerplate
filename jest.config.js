f50950d5820f18f3b13097d25645e75d2647fa74
LOÏC RICO <65566752+ricoloic@users.noreply.github.com>
2023-04-12 19:18:16 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   moduleNameMapper: {
-    '\\.svg': '<rootDir>/src/tests/mocks/mockSvg.tsx',
+    '\\.svg': '<rootDir>/src/tests/mocks/mockSVG.tsx',
   },
 };
